09478c7dcbbd7a82331040bb7178a1eaa042c5b0
lampssy <59943396+lampssy@users.noreply.github.com>
2020-01-16 02:33:55 +0100

    Update Simulation.cpp

--- Simulation.cpp
+++ Simulation.cpp
@@ -1,6 +1,6 @@
 #include "Simulation.h"
 
-Simulation::Simulation(const vector<vector<double>>& RHO, const vector<vector<vector<double>>>& U, const vector<vector<vector<double>>>& FIN, const vector<vector<int>>& C)
+Simulation::Simulation()
 {
 }
 
@@ -10,6 +10,6 @@ Simulation::~Simulation()
 
 void Simulation::iterate()
 {
-	collision();	//velocity set and nodes vector initialized while creating domain
+	collision();		//velocity set and nodes vector initialized while creating domain
 	stream();		//velocity set and nodes vector initialized while creating domain
 }
